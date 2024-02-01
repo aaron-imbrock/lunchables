93ae40665ada86b0d8d8e96c86883fc35c45d67f
Aaron Imbrock <aimbrock@gmail.com>
2024-02-01 02:43:05 -0600

    Attempting multiple changes to Ids

---
+++ title-bar/src/App-backup.js
@@ -0,0 +1,42 @@
+import './App.css';
+
+export const defaultTitle = "Work Items";
+// export const time = createRoot(document.getElementById('time'));
+
+export default function EditPost() {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    // console.log(formJson);
+    document.title = formJson.postTitle;
+    document.root = formJson.postTitle;
+  }
+
+  return (
+    <>
+    <form method="post" onSubmit={handleSubmit}>
+      <label>
+        <input name="postTitle" defaultValue={ defaultTitle } />
+      </label>
+      <button type="submit">Update This Tab</button>
+      {/* <button type="submit">Create New Tab</button> */}
+    </form>
+    </>
+  );
+}
+
+// function clock() {
+//   const element = (
+//     <div>
+//       <h4>Browser Time: {new Date().toLocaleTimeString()}</h4>
+//       <h4>UTC: {new Date().toUTCString()}</h4>
+//     </div>
+//   );
+//   time.render(element);
+// }
+
+// setInterval(clock, 1000);
--- title-bar/src/index.js
+++ title-bar/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './reset.css';
 import App from './App';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const title = ReactDOM.createRoot(document.getElementById('title'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const title = ReactDOM.createRoot(document.getElementById('title'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App targetId="root" />
+  </React.StrictMode>
+);
+
+title.render(
+  <React.StrictMode>
+    <App targetId="title" />
   </React.StrictMode>
 );
