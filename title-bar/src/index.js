67e4f30ac1ff7e437cd3e7b1ff29a46f9cfc9b0c
Aaron Imbrock <aimbrock@gmail.com>
2024-02-01 04:03:08 -0600

    Hunting css pathing in react

--- title-bar/src/index.js
+++ title-bar/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
-import './style.css';
+import 'styles/reset.css';
+import 'styles/style.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
